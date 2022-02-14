2cccc1a7e5f0b0e70a8d2d991747c15cd481650b
Ricardo Recaredo <rrecaredo@twilio.com>
2022-02-14 12:23:24 -0800

    trigger pipeline job

--- video-composer/jest.config.js
+++ video-composer/jest.config.js
@@ -12,11 +12,5 @@ module.exports = {
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
   reporters: ['default'],
-  clearMocks: true,
-  moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': require.resolve(
-      './__mocks__/fileMock.ts'
-    ),
-    '\\.(css|scss)$': require.resolve('./__mocks__/styleMock.ts'),
-  },
+  clearMocks: true
 };
